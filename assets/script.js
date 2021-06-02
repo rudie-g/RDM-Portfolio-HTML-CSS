da5eddc0d102ff731f86293a7c840f43bf595270
Rudie G <rauoldm@gmail.com>
2021-06-02 14:14:54 -0500

    Added working links to projects

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var firstCard = document.getElementById("firstCard");
+var secondCard = document.getElementById("secondCard");
+var thirdCard = document.getElementById("thirdCard");
+
+console.log(firstCard);
+console.log(secondCard);
+console.log(thirdCard);
+
+firstCard.addEventListener("click", window.open("https://rudie-g.github.io/BCBv1.0/"));
+secondCard.addEventListener("click", window.open("https://rudie-g.github.io/05-homework-third-party-apis/"));
+thirdCard.addEventListener("click", window.open("https://rudie-g.github.io/06-homework-serverside-apis/"));
